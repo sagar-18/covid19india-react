d8344cfca7d3f9b0dae8463d05db89d1b497d402
Sagar Chauhan <36581523+sagar-18@users.noreply.github.com>
2020-03-25 08:34:22 +0530

    Update cluster.js

--- src/components/cluster.js
+++ src/components/cluster.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 
 function Cluster(props) {
   return (
-    <div className="iframe-container" referrerPolicy="no-referrer" width="100%"><iframe src="https://cluster.covid19india.org"></iframe></div>
+    <div className="iframe-container" referrerPolicy="no-referrer" width="100%"><iframe src="https://bharat.coronacurfew.live"></iframe></div>
   );
 }
 
