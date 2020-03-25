01853a1069b07d5d45c66c707a64bccf8c8d8ffd
Sagar Chauhan <36581523+sagar-18@users.noreply.github.com>
2020-03-25 09:00:41 +0530

    Create global.js

---
+++ src/components/global.js
@@ -0,0 +1,9 @@
+import React, {useEffect} from 'react';
+
+function Global(props) {
+  return (
+    <div className="iframe-container" referrerPolicy="no-referrer" width="100%"><iframe src="https://www.coronacurfew.live"></iframe></div>
+  );
+}
+
+export default Global;
