9a327aa8f21e4b3551e6e5a3aff3ead78fa1e0f2
Naveen S <coolnaveen1996@gmail.com>
2020-03-26 01:18:13 +0530

    Dark mode initial commit

--- src/App.js
+++ src/App.js
@@ -15,43 +15,42 @@ const history = require('history').createBrowserHistory;
 
 function App() {
   return (
-    <div className="App">
-
-      <Router history={history}>
-        <Route render={({location}) => (
-          <div className="Almighty-Router">
-            <Navbar />
-            <Banner />
-            <Route exact path="/" render={() => <Redirect to="/" />} />
-            <Switch location={location}>
-              <Route exact path="/" render={(props) => <Home {...props}/>} />
-              <Route exact path="/links" render={(props) => <Links {...props}/>} />
-              <Route exact path="/summary" render={(props) => <Summary {...props}/>} />
-              <Route exact path="/clusters" render={(props) => <Cluster {...props}/>} />
-              <Route exact path="/faq" render={(props) => <FAQ {...props}/>} />
-            </Switch>
+    <div className="App theme--dark">
+      <div className="container">
+        <Router history={history}>
+          <Route render={({location}) => (
+            <div className="Almighty-Router">
+              <Navbar />
+              <Banner />
+              <Route exact path="/" render={() => <Redirect to="/" />} />
+              <Switch location={location}>
+                <Route exact path="/" render={(props) => <Home {...props}/>} />
+                <Route exact path="/links" render={(props) => <Links {...props}/>} />
+                <Route exact path="/summary" render={(props) => <Summary {...props}/>} />
+                <Route exact path="/clusters" render={(props) => <Cluster {...props}/>} />
+                <Route exact path="/faqs" render={(props) => <FAQ {...props}/>} />
+              </Switch>
+            </div>
+          )}
+          />
+        </Router>
+        <footer className="fadeInUp" style={{animationDelay: '2s'}}>
+          <img src="/icon.png" alt="logo"/>
+          <h5>We stand with everyone fighting on the frontlines</h5>
+          <div className="link">
+            <a href="https://github.com/covid19india">covid19india</a>
           </div>
-        )}
-        />
-      </Router>
-      <footer className="fadeInUp" style={{animationDelay: '2s'}}>
-        <img src="/icon.png" alt="logo"/>
-        <h5>We stand with everyone fighting on the frontlines</h5>
-        <div className="link">
-          <a href="https://github.com/covid19india">covid19india</a>
-
-        </div>
-        <div id='footerButtons'>
-          <a className="button" href="https://bit.ly/patientdb" target="_noblank">
-            <Icon.Database /><span>Crowdsourced Patient Database&nbsp;</span>
-          </a>
-          <a href="https://bit.ly/covid19crowd" className="button telegram" target="_noblank">
-            <Icon.MessageCircle />
-            <span>Join Telegram to Collaborate!</span>
-          </a>
-        </div>
-      </footer>
-
+          <div id='footerButtons'>
+            <a className="button" href="https://bit.ly/patientdb" target="_noblank">
+              <Icon.Database /><span>Crowdsourced Patient Database&nbsp;</span>
+            </a>
+            <a href="https://bit.ly/covid19crowd" className="button telegram" target="_noblank">
+              <Icon.MessageCircle />
+              <span>Join Telegram to Collaborate!</span>
+            </a>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
