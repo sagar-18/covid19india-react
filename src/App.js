3c86cc10257576bc5593fe851df72d1dfb4d5a79
Sagar Chauhan <36581523+sagar-18@users.noreply.github.com>
2020-03-26 16:35:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,28 +16,37 @@ import Banner from './components/banner';
 const history = require('history').createBrowserHistory;
 
 function App() {
+  const [theme, setTheme] = useState(localStorage.getItem('theme'));
+  const toggleTheme = (oldTheme) => {
+    const newTheme = oldTheme === 'dark'? 'light' : 'dark';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+  
   return (
-    <div className="App">
+    <div className={ theme == 'dark'? 'App theme--dark' : 'App theme--light' } >
+      <div className="container">
+        <Router history={history}>
+          <Route render={({location}) => (
+            <div className="Almighty-Router">
+              <Navbar theme = { theme } toggleTheme={ toggleTheme } />
+              <Banner />
+              <Route exact path="/" render={() => <Redirect to="/" />} />
+              <Switch location={location}>
+                <Route exact path="/" render={(props) => <Home {...props}/>} />
+                <Route exact path="/links" render={(props) => <Links {...props}/>} />
+                <Route exact path="/summary" render={(props) => <Summary {...props}/>} />
+                <Route exact path="/clusters" render={(props) => <Cluster {...props}/>} />
+                <Route exact path="/faqs" render={(props) => <FAQ {...props}/>} />
+                <Route exact path="/global" render={(props) => <Global {...props}/>} />
 
-      <Router history={history}>
-        <Route render={({location}) => (
-          <div className="Almighty-Router">
-            <Navbar />
-            <Banner />
-            <Route exact path="/" render={() => <Redirect to="/" />} />
-            <Switch location={location}>
-              <Route exact path="/" render={(props) => <Home {...props}/>} />
-              <Route exact path="/links" render={(props) => <Links {...props}/>} />
-              <Route exact path="/summary" render={(props) => <Summary {...props}/>} />
-              <Route exact path="/clusters" render={(props) => <Cluster {...props}/>} />
-              <Route exact path="/global" render={(props) => <Global {...props}/>} />
-              <Route exact path="/faq" render={(props) => <FAQ {...props}/>} />
+              </Switch>
+            </div>
+          )}
+          />
+        </Router>
 
-            </Switch>
-          </div>
-        )}
-        />
-      </Router>
+ 
       <footer className="fadeInUp" style={{animationDelay: '2s'}}>
         <img src="/icon.png" alt="logo"/>
         <h5>Live Dedicated Corona Tracker For India</h5>
