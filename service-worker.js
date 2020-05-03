0a32753cc8e052a537b03e8703b3a623afa1270c
sagar-18 <36581523+sagar-18@users.noreply.github.com>
2020-05-03 09:03:53 +0000

    Deploying to gh-pages from  @ 8a494d84acfdf4874cb407c3f75760f7d80eb637 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53f6beed6de71d7cf35199192dbde789.js"
+  "/precache-manifest.6fbd90f18f6d71b9ae4e839da3158050.js"
 );
 
 self.addEventListener('message', (event) => {
